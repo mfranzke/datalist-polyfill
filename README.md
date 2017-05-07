bd4cc00e9d83130aeb00725619ffc9cbf61e249d
Maximilian <mfr@nzke.net>
2017-05-08 00:28:32 +0200

    version 1.0.0
    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # datalist-polypill
-Minimal and library dependency-free vanilla JavaScript polypill for the awesome datalist-functionality
+This is a minimal and library dependency-free vanilla JavaScript polypill for the awesome datalist-functionality, that will bring joy and happiness into our lives :-)
+
+Tested in Safari, for which it's mainly meant for, as all of the others are already supporting it - quite - well: http://caniuse.com/#feat=datalist
+No dependencies, written in plain JavaScript. Released under the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+## Features
+* Lightweight: 3.02 kB of JavaScript - less than 1.2 kB gzipped
+* Fully flexible to change the data / <option>s
+* Enables core keyboard controls like e.g. the up and down arrow keys, ESC and ENTER
+* Implements the [WAI-ARIA design pattern|https://www.w3.org/TR/wai-aria-practices/]
+
+## Core concepts
+The plugin was designed with the following concepts kept in mind:
+* library dependency-free
+* DOM agnostic by event delegation
+* code accessibility / readability: because this is what developers even also should take care about regarding themselves
+
+## Installation
+Just integrate both the CSS and JavaScript file into your code - et voilà.
+
+## API
+Nothing really, just plug it in, it will work out of the box.
+
+You'd like to set a title-Attribute on the datalist HTML tag, as this would get used as label for the first, disabled entry within the polyfilling select.
+
+### dynamic HTML (or DHTML, if you like to be a little bit nostalgic)
+In case that you'd like to dynamically add or modify / build your HTML code, you're even also fine with this polyfill, as it's built with event delegation that makes your UI work with a glance - no function to call after DOM manipulation or something similar.
+
+### Changes to the available options
+If you'd like to make a change to the integrated list of <option> elements, feel free to either remove or add them right away - the list would get generated on the fly after the user typed in something into the input field, so I've even also got you covered on this.
+
+You could even also disable <option> elements by the disabled attribute if necessary.
+
+## Demo
+See the polyfill in action either by downloading / forking this repo and have a look at the demo.html, or on JSFiddle: https://jsfiddle.net/mfranzke/s6awjfze/
+
+## things to keep in mind
+* The demo HTML code is meant to be simple - I do know that things like a surrounding <form> and <fieldset> are missing. But lets focus on the relevant tags that this polyfill is all about for the demo.
+* After I thought it through and even also did some experiments, I've finally chosen the <select> element to polyfill the functionality of the datalist functionality, as it brought most of the functionality, whereas I accepted that it doesn't behave and doesn't look totally equally.
+** As I wanted to use most native elements in the most low level / simple way instead of visually emulating a list and than afterwards regain all of the functionality via a lot of JavaScript logic, I've ended up with this element, that even also knows how to play with nested <option> elements pretty well.
+** I even also tried its multiple attribute, as this is most likely even already what you're up to regarding appearance and functionality, but it does result in - surprise - the possibility for multiple selections, which isn't <datalist>s kind of thing ...
+
+## Credits
+Supported by Christian and Johannes.
+
+## Changelog
+### Version 1.0.0 - 2017/05/04
+First release.
+
+## Outro
+Personally I even also do like the "keep it simple" approach provided within the W3C specs even already:
+https://www.w3.org/TR/html5/forms.html#the-datalist-element
+
+But on the other hand this leads to an additional visible field and doesn't emulate the (hopefully, fingers crossed) upcoming x-browser implementation and leaves unnecessary syntax for all of the clients that wouldn't even need it (anymore).
---
+++ datalist-polyfill.js
@@ -0,0 +1,237 @@
+/*
+* datalist-polyfill.js - https://github.com/mfranzke/datalist-polypill
+* @license Copyright(c) 2017 by Maximilian Franzke
+* Supported by Christian and Johannes - many thanks for that !
+*//*
+* A lightweight and library dependency free vanilla JavaScript datalist polyfill.
+* Tests for native support of an inputs elements datalist functionality.
+* Elsewhere the functionality gets emulated by a select element.
+*/
+
+(function() {
+	'use strict';
+
+	// feature detection
+	var nativedatalist = !!( 'list' in document.createElement( 'input' ) ) && 
+		!!( document.createElement( 'datalist' ) && window.HTMLDataListElement );
+
+	// in case of that the feature doesn't exist, emulate it's functionality
+	if ( !nativedatalist ) {
+
+		// function regarding the inputs interactions
+		var inputInputList = function( event ) {
+
+			var $eventTarget = event.target;
+
+			// check for whether the events target was an input datalist
+			if ( $eventTarget.matches( 'input[list]' ) ) {
+
+				var list = $eventTarget.getAttribute( 'list' ),
+					$dataList = document.getElementById( list );
+
+				// still check for an existing instance
+				if ( $dataList !== null ) {
+
+					var $dataListSelect = $dataList.getElementsByTagName('select')[0];
+
+					// still check for an existing instance
+					if ( $dataListSelect !== undefined ) {
+
+						// on an ESC key press within the input, let's break here and hide the select
+						if ( event.keyCode === 27 ) {
+							$dataListSelect.classList.remove( 'visible' );
+							$dataListSelect.setAttribute( 'aria-hidden', true );
+
+							return;
+						}
+
+						var $dataListOptions = $dataList.querySelectorAll( 'option[value]' ),
+							inputValue = $eventTarget.value.toLowerCase(),
+							newSelectValues = document.createDocumentFragment(),
+							disabledValues = document.createDocumentFragment(),
+							visible = false;
+
+						// if the input contains a value, than ...
+						if ( inputValue !== "" ) {
+
+							// ... sort all entries and
+							var nodeArray = Array.prototype.slice.call( $dataListOptions );
+
+							nodeArray.sort( function( a, b ) {
+							    return a.value.localeCompare( b.value );
+							}).forEach( function( opt ) {
+								var optionValue = opt.value;
+
+								// ... put this option into the fragment that is meant to get inserted into the select
+								// "Each option element that is a descendant of the datalist element, that is not disabled, and whose value is a string that isn't the empty string, represents a suggestion. Each suggestion has a value and a label." (W3C)
+							    if ( optionValue !== "" && optionValue.toLowerCase().indexOf( inputValue ) !== -1 && opt.disabled === false ) {
+								    opt.innerText = optionValue;
+
+								    newSelectValues.appendChild( opt );
+
+								    // ... and set the state of the select to get displayed in that case
+								    visible = true;
+								} else {
+									// ... or put this option that isn't relevant to the users into the fragment that is supposed to get inserted outside of the select
+									disabledValues.appendChild( opt );
+								}
+							});
+
+							// input the options fragment into the datalists select
+							$dataListSelect.appendChild( newSelectValues );
+
+							// input the unused options as siblings next to the select
+							$dataList.appendChild( disabledValues );
+						}
+
+						// toggle the visibility of the select according to previous checks
+						$dataListSelect.classList.toggle( 'visible', visible );
+						$dataListSelect.setAttribute( 'aria-hidden', !visible );
+
+						// on arrow up or down keys, focus the select
+						if ( event.keyCode === 38 || event.keyCode === 40 ) {
+
+							$dataListSelect.focus();
+						}
+					}
+				}
+			}
+		};
+
+		// focus or blur events
+		var changesInputList = function( event ) {
+
+			var $eventTarget = event.target;
+
+			// check for whether the events target was an input datalist
+			if ( $eventTarget.matches( 'input[list]' ) ) {
+
+				var eventType = event.type,
+					list = $eventTarget.getAttribute( 'list' ),
+					$dataList = document.getElementById( list );
+
+				// still check for an existing instance
+				if ( $dataList !== null ) {
+
+					var $dataListSelect = $dataList.getElementsByTagName('select')[0],
+						// either have the select set to the state to get displayed in case of that it would have been focused or because it's the target on the inputs blur
+						visible = ( ( eventType === "focus" && event.target.value !== "" ) || ( event.relatedTarget && event.relatedTarget === $dataListSelect ) ),
+						message = $dataList.title;
+
+					// creating the select if there's no instance so far (e.g. because of that it hasn't been handled or it has been dynamically inserted)
+					if ( $dataListSelect === undefined ) {
+						var rects = $eventTarget.getClientRects(),
+							$message = document.createElement('option');
+
+						$dataListSelect = document.createElement( 'select' );
+						
+						// WAI ARIA attributes
+						$dataListSelect.setAttribute( 'aria-hidden', true );
+						$dataListSelect.setAttribute( 'aria-live', 'polite' );
+						$dataListSelect.setAttribute( 'role', 'listbox' );
+
+						// the select should get positioned underneath the input field ...
+						$dataListSelect.style.marginLeft = "-" + rects[0].width + "px";
+						$dataListSelect.style.marginTop = rects[0].height + 'px';
+						$dataListSelect.style.minWidth = rects[0].width + 'px';
+
+						// ... and it's first entry should contain the localized message to select an entry
+						$message.innerText = message;
+						// ... and disable this option, as it shouldn't get selected by the user
+						$message.disabled = true;
+						// ... and finally insert it into the select
+						$dataListSelect.appendChild( $message );
+
+						// add select to datalist element ...
+						$dataList.appendChild( $dataListSelect );
+
+						// ... and our upfollowing function to the change event
+						$dataListSelect.addEventListener( 'change', changeDataListSelect );
+						$dataListSelect.addEventListener( 'blur', changeDataListSelect );
+						$dataListSelect.addEventListener( 'keyup', changeDataListSelect );
+
+
+						// plus we'd like to prevent autocomplete on the input datalist field
+						$eventTarget.setAttribute( 'autocomplete', 'off' );
+						
+						// WAI ARIA attributes
+						$eventTarget.setAttribute( 'role', 'textbox' );
+						$eventTarget.setAttribute( 'aria-haspopup', 'true' );
+						$eventTarget.setAttribute( 'aria-autocomplete', 'list' );
+						$eventTarget.setAttribute( 'aria-owns', list );
+					}
+
+					// toggle the visibility of the select according to previous checks
+					$dataListSelect.classList.toggle( 'visible', visible );
+					$dataListSelect.setAttribute( 'aria-hidden', !visible );
+					
+					// bind the keyup event on the related dalalists input
+					switch( eventType ) {
+						case "focus":
+							$eventTarget.addEventListener( 'keyup', inputInputList );
+							
+							$eventTarget.addEventListener( 'blur', changesInputList, true );
+						break;
+						case "blur":
+							$eventTarget.removeEventListener( 'keyup', inputInputList );
+							
+							$eventTarget.removeEventListener( 'blur', changesInputList, true );
+						break;
+					}
+				}
+			}
+		};
+
+		// function regarding changes to the datalist polyfilling created select
+		var changeDataListSelect = function( event ) {
+
+			var $eventTarget = event.target,
+				message = $eventTarget.parentNode.title;
+
+			// check for whether the events target was a select
+			if ( $eventTarget.matches( 'select' ) ) {
+
+				var eventType = event.type,
+					// ENTER and ESC keys
+					visible = ( eventType === "keyup" && ( event.keyCode !== 13 && event.keyCode !== 27 ) );
+				
+				// change event or enter key
+				if ( eventType === "change" || ( eventType === "keyup" && event.keyCode === 13 ) ) {
+
+					var list = $eventTarget.parentNode.id,
+						$inputList = document.querySelector('input[list="' + list + '"]'),
+						eventTargetValue = $eventTarget.value;
+
+					// input the selects value into the input on a change within the list
+					if ( $inputList !== null && eventTargetValue.length > 0 && eventTargetValue !== message ) {
+						$inputList.value = eventTargetValue;
+					}
+				}
+
+				// toggle the visibility of the select according to previous checks
+				$eventTarget.classList.toggle( 'visible', visible );
+				$eventTarget.setAttribute( 'aria-hidden', !visible );
+			}
+		};
+
+		// binding the focus event - matching the input[list]s happens in the function afterwards
+		document.addEventListener( 'focus', changesInputList, true );
+
+
+		// matches() polyfill - just in case ...
+		if (!Element.prototype.matches) {
+		    Element.prototype.matches = 
+		        Element.prototype.matchesSelector || 
+		        Element.prototype.mozMatchesSelector ||
+		        Element.prototype.msMatchesSelector || 
+		        Element.prototype.oMatchesSelector || 
+		        Element.prototype.webkitMatchesSelector ||
+		        function(s) {
+		            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+		                i = matches.length;
+		            while (--i >= 0 && matches.item(i) !== this) {}
+		            return i > -1;
+		        };
+		}
+	}
+})();
