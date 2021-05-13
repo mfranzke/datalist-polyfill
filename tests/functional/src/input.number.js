8cadb70253deb9b6b3d737f95fa5bf1661ba28b4
Maximilian <787658+mfranzke@users.noreply.github.com>
2021-05-13 15:31:32 +0200

    refactor: run xo --fix

--- tests/functional/src/input.number.js
+++ tests/functional/src/input.number.js
@@ -1,3 +1,3 @@
-var field = { fieldId: 'number', initialValue: '19', expectedAmount: 28, wrongValue: '99' };
+const field = { fieldId: 'number', initialValue: '19', expectedAmount: 28, wrongValue: '99' };
 
 // @codekit-append 'tests.js';
